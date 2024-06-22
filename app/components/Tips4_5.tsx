5f48884c66ddc4e4224b4c32a3a1f4e907bb654f
Shin-sibainu <shincode0712@gmail.com>
2024-06-22 12:38:09 +0900

    momd

---
+++ app/components/Tips4_5.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Tips4_5 = () => {
+  return (
+    <div>
+      {/* <div className="sample_css">Subscription</div> */}
+
+      <div>{/* <div className="w-20 h-10 rounded-lg"></div> */}</div>
+    </div>
+  );
+};
+
+export default Tips4_5;
