7f1dec6d130ce12a84f08bdd10d8e695ec3ce0bc
Yuki <diesunabe@gmail.com>
2024-07-07 23:38:34 +0900

    complete

--- app/components/Tips1_2.tsx
+++ app/components/Tips1_2.tsx
@@ -3,12 +3,12 @@ import React from "react";
 const Tips1_2 = () => {
   return (
     <div className="flex gap-4">
-      <div className="cursor-pointer grid place-items-center h-40 w-40 bg-blue-400">
-        <div className="h-10 w-10 bg-black"></div>
-        <div className="h-10 w-10 bg-black"></div>
+      <div className="peer group cursor-pointer grid place-items-center h-40 w-40 bg-blue-400 hover:bg-purple-400">
+        <div className="h-10 w-10 bg-black group-hover:bg-red-600"></div>
+        <div className="h-10 w-10 bg-black group-hover:bg-blue-600"></div>
       </div>
 
-      {/* <div className="h-40 w-40 bg-green-400"></div> */}
+      <div className="h-40 w-40 bg-green-400 peer-hover:bg-orange-500"></div>
     </div>
   );
 };
