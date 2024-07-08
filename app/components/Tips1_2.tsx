5849662886669de8c2134e1c5fda8fadf7cc8f35
Yuki <diesunabe@gmail.com>
2024-07-09 00:17:12 +0900

    animations

--- app/components/Tips1_2.tsx
+++ app/components/Tips1_2.tsx
@@ -3,12 +3,12 @@ import React from "react";
 const Tips1_2 = () => {
   return (
     <div className="flex gap-4">
-      <div className="peer group cursor-pointer grid place-items-center h-40 w-40 bg-blue-400 hover:bg-purple-400">
-        <div className="h-10 w-10 bg-black group-hover:bg-red-600"></div>
-        <div className="h-10 w-10 bg-black group-hover:bg-blue-600"></div>
+      <div className="peer group cursor-pointer grid place-items-center h-40 w-40 bg-blue-400 hover:bg-purple-400 transition-colors duration-500 ease-in-out">
+        <div className="h-10 w-10 bg-black group-hover:bg-red-600 transition-colors duration-500 ease-in-out"></div>
+        <div className="h-10 w-10 bg-black group-hover:bg-blue-600 transition-colors duration-500 ease-in-out"></div>
       </div>
 
-      <div className="h-40 w-40 bg-green-400 peer-hover:bg-orange-500"></div>
+      <div className="h-40 w-40 bg-green-400 peer-hover:bg-orange-500 transition-colors duration-500 ease-in-out animate-spin"></div>
     </div>
   );
 };
