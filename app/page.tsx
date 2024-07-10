4f9ca80f874dc2a203b91373216c32af5f99fc55
Yuki <diesunabe@gmail.com>
2024-07-10 23:17:33 +0900

    size/space

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
 import Tips1_2 from "./components/Tips1_2";
 import Tips3 from "./components/Tips3";
 import Tips4_5 from "./components/Tips4_5";
+import Tips6 from "./components/Tips6";
 
 export default function Home() {
   return (
     <main className="py-20 px-20">
       {/* <Tips1_2 /> */}
       {/* <Tips3 /> */}
-      <Tips4_5 />
+      {/* <Tips4_5 /> */}
+      <Tips6/>
     </main>
   );
 }
